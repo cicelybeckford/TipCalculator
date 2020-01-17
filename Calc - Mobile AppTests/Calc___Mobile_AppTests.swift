d46755d686a41424562cf0ab76ef5efbb24daecd
cicelybeckford <cicelybeckford@users.noreply.github.com>
2020-01-17 11:04:16 -0500

    Initial Commit

---
+++ Calc - Mobile AppTests/Calc___Mobile_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Calc___Mobile_AppTests.swift
+//  Calc - Mobile AppTests
+//
+//  Created by Cicely Beckford on 1/17/20.
+//  Copyright © 2020 Cicely Beckford. All rights reserved.
+//
+
+import XCTest
+@testable import Calc___Mobile_App
+
+class Calc___Mobile_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
